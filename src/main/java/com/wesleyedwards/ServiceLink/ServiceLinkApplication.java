711dac7c637aa032c65207efdbf215def4e288cf
wredwards94 <wredwards94@outlook.com>
2024-12-23 20:17:14 -0500

    Initial commit

---
+++ src/main/java/com/wesleyedwards/ServiceLink/ServiceLinkApplication.java
@@ -0,0 +1,13 @@
+package com.wesleyedwards.ServiceLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleyedwards/ServiceLink/ServiceLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyedwards.ServiceLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
